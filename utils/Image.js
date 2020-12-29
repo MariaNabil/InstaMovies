4103f2a94fbec1d5f9fd33363cc746207763e8ab
MariaNabil <maria.nabilfcis@hotmail.com>
2020-12-29 20:20:15 +0200

    Edit ImagePicker for ios

--- utils/Image.js
+++ utils/Image.js
@@ -1,32 +1,49 @@
-// import ImagePicker from 'react-native-image-picker';
-import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import ImagePicker, {launchImageLibrary} from 'react-native-image-picker';
 
 const options = {
   title: 'Select',
   cancelButtonTitle: 'Cancel',
-  takePhotoButtonTitle: 'Camera',
   chooseFromLibraryButtonTitle: 'Library',
   storageOptions: {
     skipBackup: true,
-    path: 'RoxiitAdmin',
+    path: 'InstaMovies',
   },
   noData: true,
   mediaType: 'photo',
   allowsEditing: true,
 };
+// export const showImagePicker = (onSuccess, onFailure) => {
+
 export const showImagePicker = (onSuccess, onFailure) => {
-  launchImageLibrary(options, (response) => {
-    if (response.didCancel) {
-      onFailure && onFailure(null);
-    } else if (response.error) {
-      onFailure && onFailure(response.error);
-    } else if (response.customButton) {
-      // todo when needed
-      onFailure && onFailure(response.customButton);
-    } else if (response) {
-      onSuccess && onSuccess(response);
-    } else {
-      onFailure && onFailure(response);
-    }
-  });
+  if (Platform.OS === 'ios') {
+    ImagePicker.showImagePicker(options, (response) => {
+      if (response.didCancel) {
+        onFailure && onFailure(null);
+      } else if (response.error) {
+        onFailure && onFailure(response.error);
+      } else if (response.customButton) {
+        // todo when needed
+        onFailure && onFailure(response.customButton);
+      } else if (response) {
+        onSuccess && onSuccess(response);
+      } else {
+        onFailure && onFailure(response);
+      }
+    });
+  } else {
+    launchImageLibrary(options, (response) => {
+      if (response.didCancel) {
+        onFailure && onFailure(null);
+      } else if (response.error) {
+        onFailure && onFailure(response.error);
+      } else if (response.customButton) {
+        // todo when needed
+        onFailure && onFailure(response.customButton);
+      } else if (response) {
+        onSuccess && onSuccess(response);
+      } else {
+        onFailure && onFailure(response);
+      }
+    });
+  }
 };
